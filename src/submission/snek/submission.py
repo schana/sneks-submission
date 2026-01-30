bf19e449ee06a79dbc8dc8c1b73cf1d66da67abf
Nathaniel Schaaf <schana@users.noreply.github.com>
2026-01-30 16:00:05 -0500

    Clean repo

--- src/submission/snek/submission.py
+++
@@ -1,7 +0,0 @@
-from sneks.core.direction import Direction
-from sneks.interface.snek import Snek
-
-
-class CustomSnek(Snek):
-    def get_next_direction(self) -> Direction:
-        return Direction.UP
